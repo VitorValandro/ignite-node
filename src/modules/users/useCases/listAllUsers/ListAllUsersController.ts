1c791834b738c600f6aff809752c79a69330ce2f
Vitor Valandro <vitormateusd@gmail.com>
2022-03-08 09:28:54 -0300

    Handle exceptions in resources' responses

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -7,9 +7,12 @@ class ListAllUsersController {
 
   handle(request: Request, response: Response): Response {
     const { user_id } = request.headers;
-    const users = this.listAllUsersUseCase.execute({ user_id });
-
-    return response.status(200).json(users);
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
+      return response.status(200).json(users);
+    } catch (err) {
+      return response.status(400).json({ error: err });
+    }
   }
 }
 
